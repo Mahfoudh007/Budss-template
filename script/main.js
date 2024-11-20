57c3472572a8f4d47bea808c6e4e6d80b245b838
mahfoudh <mahfoodh20015@gmail.com>
2024-11-20 16:27:33 +0300

    added cookies

--- script/main.js
+++ script/main.js
@@ -1,12 +1,41 @@
-// for burger menu
+// ######  for burger menu
 
-const burgerMenu = document.getElementById('burgerMenu')
-const burgerClose = document.getElementById('burgerClose')
-const burgerOpen = document.getElementById('burgerOpen')
+const burgerMenu = document.getElementById("burgerMenu");
+const burgerClose = document.getElementById("burgerClose");
+const burgerOpen = document.getElementById("burgerOpen");
 
-burgerOpen.addEventListener('click', function () {
-    burgerMenu.classList.add('active');
-})
-burgerClose.addEventListener('click', function () {
-    burgerMenu.classList.remove('active');
-})
+burgerOpen.addEventListener("click", function () {
+  burgerMenu.classList.add("active");
+});
+burgerClose.addEventListener("click", function () {
+  burgerMenu.classList.remove("active");
+});
+
+// ######## for Cookies
+
+// Get elements
+const cookiesBanner = document.getElementById("cookiesBanner");
+const acceptBtn = document.getElementById("acceptCookies");
+const declineBtn = document.getElementById("declineCookies");
+const closeBanner = document.getElementById("closeBanner");
+
+// When the "Accept Cookies" button is clicked
+acceptBtn.addEventListener("click", () => {
+    alert("Cookies Accepted!");
+  cookiesBanner.style.display = "none";
+});
+
+// When the "Decline Cookies" button is clicked
+declineBtn.addEventListener("click", () => {
+  alert("Cookies Declined!");
+  cookiesBanner.style.display = "none";
+
+});
+
+// When the "Close" button is clicked
+closeBanner.addEventListener("click", () => {
+  cookiesBanner.style.display = "none";
+});
+
+
+
